bd177cb06ea76615eda96f900ddde791bfba3f39
jabalpureishan <115293847+jabalpureishan@users.noreply.github.com>
2023-04-01 23:46:14 +0530

    Added solution - LeetHub

---
+++ Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    def searchInSorted(self,arr, N, K):
+        if K in arr:
+            return 1
+        return -1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+if __name__ == '__main__': 
+	t = int(input())
+	for _ in range(t):
+		NK = input().strip().split()
+		N = int(NK[0])
+		K = int(NK[1])
+		A = [ int(x) for x in input().strip().split() ]
+		ob=Solution()
+		print(ob.searchInSorted(A,N,K))
+
+# } Driver Code Ends
