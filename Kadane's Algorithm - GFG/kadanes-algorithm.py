2b839a9ec1f9bbea9ac4f2086e05028e9dcb4d21
jabalpureishan <115293847+jabalpureishan@users.noreply.github.com>
2023-04-01 23:40:22 +0530

    Added solution - LeetHub

---
+++ Kadane's Algorithm - GFG/kadanes-algorithm.py
@@ -0,0 +1,43 @@
+class Solution:
+    def maxSubArraySum(self,arr,N):
+        Max_Sum= arr[0]
+        curr = arr[0]
+        for i in range(1,N):
+            if (curr + arr[i])>arr[i] :
+                curr += arr[i]
+            else:
+                curr = arr[i]
+            if curr>Max_Sum :
+                Max_Sum = curr
+        return Max_Sum
+                
+                
+
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+ 
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            
+            ob=Solution()
+            
+            print(ob.maxSubArraySum(arr,n))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
