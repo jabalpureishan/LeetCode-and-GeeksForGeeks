ecbc8a2283aad16480fa167088ad308163bc1a22
jabalpureishan <115293847+jabalpureishan@users.noreply.github.com>
2023-04-04 00:42:41 +0530

    Added solution - LeetHub

---
+++ Merge Sort - GFG/merge-sort.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    def mergeSort(self,arr, l, r):
+        arr.sort()
+        return arr
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+import sys
+input = sys.stdin.readline
+if __name__ == "__main__":
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        arr=list(map(int,input().split()))
+        Solution().mergeSort(arr, 0, n-1)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
+# } Driver Code Ends
