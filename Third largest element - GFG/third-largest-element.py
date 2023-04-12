e2f2c4a13da426e286f6ad442f71916d9e137e9c
jabalpureishan <115293847+jabalpureishan@users.noreply.github.com>
2023-04-12 23:51:32 +0530

    Added solution - LeetHub

---
+++ Third largest element - GFG/third-largest-element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def thirdLargest(self,a, n):
+        a.sort(reverse = True)
+        return a[2]
+
+
+#{ 
+ # Driver Code Starts
+# Your code goes here
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        print(Solution().thirdLargest(arr, n))
+# } Driver Code Ends
