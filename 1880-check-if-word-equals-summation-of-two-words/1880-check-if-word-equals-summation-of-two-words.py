dd32c2f4c16cf61ce8e111eebbc50706b752d7fa
jabalpureishan <115293847+jabalpureishan@users.noreply.github.com>
2023-04-27 21:28:08 +0530

    Time: 43 ms (7.96%), Space: 16.3 MB (12.63%) - LeetHub

---
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+        D = {"a":0,"b":1,"c":2,"d":3,"e":4,"f":5,"g":6,"h":7,"i":8,"j":9,"k":10,"l":11,"m":12,"n":13,"o":14,"p":15,"q":16,"r":17,"s":18,"t":19,"u":20,"v":21,"w":22,"x":23,"y":24,"z":25,}
+        first,second,target = "","",""
+        for i in firstWord:
+            first += str(D.get(i))
+        for i in secondWord:
+            second += str(D.get(i))
+        for i in targetWord:
+            target += str(D.get(i))
+        """
+        print("first:",first,"second:",second,"target:",target)
+        """
+        if (int(first) + int(second))==int(target) :
+            return True
+        return False
