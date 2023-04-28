2186ad06c17acfe2588e5793adc807ac8d45cf8c
jabalpureishan <115293847+jabalpureishan@users.noreply.github.com>
2023-04-28 13:03:14 +0530

    Time: 61 ms (40.95%), Space: 16.3 MB (21.22%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -3,11 +3,13 @@ class Solution:
         new = set()
         first = "".join(sorted(words[0]))
         check = [first]
+        check = deque(check)
         out = [words[0]]
         for i in range(1,len(words)):
             pick = words[i]
             res = "".join(sorted(pick))
-            if res!=check[i-1]:
+            if res!=check[0]:
                 out.append(pick)
             check.append(res)
+            check.popleft()
         return out
