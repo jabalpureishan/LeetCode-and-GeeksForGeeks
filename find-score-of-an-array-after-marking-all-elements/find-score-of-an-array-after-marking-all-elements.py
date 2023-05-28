bc2ee68f501b49a9b5ae9430ec9f636af22fc09c
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-05-28 07:59:58 +0530

    Time: 1329 ms (60.80%) | Memory: 98.1 MB (5.96%) - LeetSync

--- find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
+++ find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
@@ -13,18 +13,19 @@ class Solution:
         mark = set()
         score = 0
         index = None
-        while(len(mark)<length):
-            for i in new:
-                index = (d.get(i))[0]
-                d.get(i).popleft()
-                if index not in mark:
-                    score += i
-                    mark.add(index)
-                    if index==0:
-                        mark.add(index+1)
-                    elif index==(length-1):
-                        mark.add(index-1)
-                    else:
-                        mark.add(index+1)
-                        mark.add(index-1)
+        for i in new:
+            index = (d.get(i))[0]
+            d.get(i).popleft()
+            if index not in mark:
+                score += i
+                mark.add(index)
+                if index==0:
+                    mark.add(index+1)
+                elif index==(length-1):
+                    mark.add(index-1)
+                else:
+                    mark.add(index+1)
+                    mark.add(index-1)
+            if len(mark)==length:
+                break
         return score
