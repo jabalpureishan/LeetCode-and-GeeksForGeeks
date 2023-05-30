fdb2c51e9fb00cbbe9161c9e0c4f6e97b72161b5
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-05-30 23:00:55 +0530

    Delete CodeForces directory

--- CodeForces/Checking.py
+++
@@ -1,8 +0,0 @@
-T = int(input(""))
-s = "codeforces"
-for i in range(T):
-    N = input("")
-    if N in s:
-        print("YES")
-    else:
-        print("NO")
--- CodeForces/HQ9+.py
+++
@@ -1,7 +0,0 @@
-I = input("")
-for i in I:
-    if((i=="H") or (i=="Q") or (i=="9")):
-        print("YES")
-        break
-else:
-    print("NO")
