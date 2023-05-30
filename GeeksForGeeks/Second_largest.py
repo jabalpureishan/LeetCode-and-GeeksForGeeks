4a1ed6285bd6b64c3e78866a5128a705ce3d4034
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-05-30 23:01:11 +0530

    Delete GeeksForGeeks directory

--- GeeksForGeeks/Second_largest.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def second(n,arr):
-        m = max(arr)
-        mx = arr[0]
-        for i in range(n):
-            pick = arr[i]
-            if pick!=m:
-                mx = max(mx,pick)
-        return mx
--- GeeksForGeeks/count_alphabets.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def count(S):
-        count = 0
-        for i in S:
-            if i.isalpha():
-                count += 1
-        return count
-    print(count("adjfjh23"))
-    print(count("n0ji#k$"))
