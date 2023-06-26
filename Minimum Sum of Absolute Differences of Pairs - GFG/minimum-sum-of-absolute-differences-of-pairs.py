daa219bf822445780bd96daab63dc2b6cccf7d5d
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-06-26 20:30:58 +0530

    Added solution - LeetHub

---
+++ Minimum Sum of Absolute Differences of Pairs - GFG/minimum-sum-of-absolute-differences-of-pairs.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    def findMinSum(self, A,B,N):
+        A.sort()
+        B.sort()
+        s = 0
+        for i in range(N):
+            s += abs(A[i]-B[i])
+        return s
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        A = list(map(int, input().strip().split()))
+        B = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.findMinSum(A,B,N)
+        print(ans)
+# } Driver Code Ends
