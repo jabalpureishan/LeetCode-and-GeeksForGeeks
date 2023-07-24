f9f5abfa8b0533a6b6d917f4073b0c25e0820f6a
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-07-24 19:45:39 +0530

    Time: 2332 ms (59.68%) | Memory: 16.3 MB (40.32%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
@@ -1,17 +1,14 @@
 class Solution:
     def beautySum(self, s: str) -> int:
-        length = len(s)
         total = 0
-        for i in range(2,length+1):
-            window = i
-            slides = length - window
+        for i in range(2,len(s)+1):
             d = {}
-            for j in s[:window]:
+            for j in s[:i]:
                 d[j] = d.get(j,0) +1
             total += max(d.values()) - min(d.values()) 
-            for j in range(slides):
-                d[s[window]] = d.get(s[window],0) + 1
-                window+=1
+            for j in range(len(s)-i):
+                d[s[i]] = d.get(s[i],0) + 1
+                i+=1
                 d[s[j]] -= 1
                 if d[s[j]]==0:
                     d.pop(s[j])
