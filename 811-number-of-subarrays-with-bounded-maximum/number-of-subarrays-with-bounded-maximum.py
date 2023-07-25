6f92679e2ca95405e4db9d866265f23599fb1d39
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-07-26 00:33:53 +0530

    Time: 580 ms (15.69%) | Memory: 25 MB (6.28%) - LeetSync

--- 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.py
+++ 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.py
@@ -1,24 +1,18 @@
 from sortedcontainers import SortedList
 class Solution:
     def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
-        def co(nums,k):
-            def maxmap(hashmap):
-                if len(hashmap)==0:
-                    return 0
-                return max(hashmap)
-            left,count,hashmap,Max = 0,0,{},-1
-            for right in range(len(nums)):
-                hashmap[nums[right]] = hashmap.get(nums[right],0) + 1
-                Max = max(Max,nums[right])
-                while(Max>k and left<right):
-                    hashmap[nums[left]] -= 1
-                    if hashmap[nums[left]]==0:
-                        hashmap.pop(nums[left])
-                    if hashmap.get(Max,-1)==-1:
-                        Max = maxmap(hashmap)
-                    left += 1
-                if Max<=k:
-                    count += right - left + 1
-            return count
-
-        return co(nums,right) - co(nums,left-1)
+        def totalSubarrays(nums, n, k):
+            ans = 0
+            i = 0
+            while (i < n):
+                if (nums[i] > k):
+                    i += 1
+                    continue
+                count = 0
+                while (i < n and nums[i] <= k):
+                    i += 1
+                    count += 1
+                ans += ((count * (count + 1)) // 2)
+            return ans
+        length = len(nums)
+        return totalSubarrays(nums,length,right) - totalSubarrays(nums,length,left-1)
