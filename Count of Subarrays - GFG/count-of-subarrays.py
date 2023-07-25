f2603a542c1fbadda5ca8baf3c96b342cc63d8ab
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-07-26 00:56:41 +0530

    Added solution - LeetHub

--- Count of Subarrays - GFG/count-of-subarrays.py
+++ Count of Subarrays - GFG/count-of-subarrays.py
@@ -3,20 +3,16 @@ class Solution:
 
 
 	def countSubarray(self,arr, n, k):
-        count,left,Max,hmap = 0,0,0,{}
-        for right in range(len(arr)):
-            hmap[arr[right]] = hmap.get(arr[right],0) + 1
-            Max = max(Max,arr[right])
-            while(Max>k and left<right):
-                hmap[arr[left]] -= 1
-                if hmap[arr[left]]==0:
-                    hmap.pop(arr[left])
-                    if arr[left]==Max:
-                        Max = max(hmap)
-                left += 1
-            if Max<=k:
-                count += right - left + 1
-        return (n*(n+1))//2 - count
+        arr.append(float("inf"))
+        total = 0
+        count = 0
+        for i in range(n+1):
+            if arr[i]<=k:
+                count += 1
+            else:
+                total += (count*(count+1))//2
+                count = 0
+        return (n*(n+1))//2 - total
 
 
 #{ 
