48268f4e682fbfa6d0eea7182af260d8ae1ebedc
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-07-26 20:52:34 +0530

    Time: 470 ms (100.00%) | Memory: 24.3 MB (99.54%) - LeetSync

--- 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.py
+++ 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.py
@@ -1,18 +1,14 @@
-from sortedcontainers import SortedList
 class Solution:
     def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
-        def totalSubarrays(nums, n, k):
-            ans = 0
-            i = 0
-            while (i < n):
-                if (nums[i] > k):
-                    i += 1
-                    continue
-                count = 0
-                while (i < n and nums[i] <= k):
-                    i += 1
+        def count(nums,k):
+            nums.append(float("inf"))
+            total = count = 0
+            for i in range(len(nums)):
+                if nums[i]<=k:
                     count += 1
-                ans += ((count * (count + 1)) // 2)
-            return ans
-        length = len(nums)
-        return totalSubarrays(nums,length,right) - totalSubarrays(nums,length,left-1)
+                else:
+                    total += (count*(count+1))//2
+                    count = 0
+            return total
+
+        return count(nums,right) - count(nums,left-1)
