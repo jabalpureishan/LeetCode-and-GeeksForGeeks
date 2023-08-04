491034ab14809f52130cabd124e6db4170f8bfef
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-08-04 10:34:55 +0530

    Time: 171 ms (96.69%) | Memory: 16.4 MB (49.47%) - LeetSync

--- 2877-shortest-string-that-contains-three-strings/shortest-string-that-contains-three-strings.py
+++ 2877-shortest-string-that-contains-three-strings/shortest-string-that-contains-three-strings.py
@@ -1,3 +1,4 @@
+
 class Solution:
     def minimumString(self, a: str, b: str, c: str) -> str:
         def merge(first,second):
@@ -11,6 +12,19 @@ class Solution:
             else:
                 first += second
             return first
-        results = (merge(merge(a,b),c),merge(merge(a,c),b),merge(merge(b,a),c), merge(merge(b,c),a),merge(merge(c,a),b),merge(merge(c,b),a),)
-        out = tuple(filter(lambda x:len(x)==(len(min(results,key=lambda x:len(x)))),results))
+        results = [
+            merge(merge(a,b),c),
+            merge(merge(a,c),b),
+            merge(merge(b,a),c),
+            merge(merge(b,c),a),
+            merge(merge(c,a),b),
+            merge(merge(c,b),a),
+        ]
+        #print("res",results)
+        Minlength = len(min(results,key=lambda x:len(x)))
+        out = []
+        for i in results:
+            if len(i)==Minlength:
+                out.append(i)
+        #print(out)
         return min(out)
