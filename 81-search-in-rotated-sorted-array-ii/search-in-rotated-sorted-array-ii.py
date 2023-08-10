ce98051468ac9f900fe454aacf66eaa9ccbf4614
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-08-10 11:42:50 +0530

    Time: 60 ms (81.88%) | Memory: 16.7 MB (96.84%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,6 +1,3 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        if target in nums:
-            return True
-        else:
-            return False
+        return target in nums
