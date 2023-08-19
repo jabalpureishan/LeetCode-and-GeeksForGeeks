fe1bf6cec1880cf6f481a86f7a3b3ea93f311597
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-08-20 00:49:06 +0530

    Time: 611 ms (87.88%) | Memory: 31.2 MB (89.90%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
@@ -2,7 +2,7 @@ class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         nums.sort()
         length = len(nums)
-        def lessthan(nums,k):
+        def lessthanequalto(nums,k):
             count,left,right = 0,0,length-1
             while(left<right):
                 Sum = nums[left] + nums[right]
@@ -11,7 +11,6 @@ class Solution:
                     left += 1
                 else:
                     right -= 1
-            #print(count)
             return count
 
-        return lessthan(nums,upper) - lessthan(nums,lower-1)
+        return lessthanequalto(nums,upper) - lessthanequalto(nums,lower-1)
