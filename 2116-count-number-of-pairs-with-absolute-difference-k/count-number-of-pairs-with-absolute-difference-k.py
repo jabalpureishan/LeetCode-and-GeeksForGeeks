7841fdf08b4093388cdf32e9be6e214420b8ca6f
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-08-25 00:10:21 +0530

    Time: 67 ms (88.24%) | Memory: 16.4 MB (39.91%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.py
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.py
@@ -1,15 +1,10 @@
 class Solution:
     def countKDifference(self, nums: List[int], k: int) -> int:
-        def func(nums,k):
-            nums.sort()
-            left,right,count = 0,1,0
-            while(right<len(nums)):
-                diff = nums[right] - nums[left]
-                if diff>k:
-                    left += 1
-                    diff = nums[right] - nums[left]
-                else:
-                    count += right - left
-                    right += 1
-            return count
-        return func(nums,k) - func(nums,k-1)
+        nums.sort()
+        d,count = {},0
+        for i in nums:
+            if i in d:
+                count += d[i]
+            d[i+k] = d.get(i+k,0) + 1
+            #d[k-i] = d.get(k-i,0) + 1
+        return count
