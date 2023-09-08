c0773b962a37fd33daebcee10886cb1b3c362268
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-09-08 14:49:41 +0530

    Time: 36 ms (86.28%) | Memory: 16.3 MB (37.62%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,25 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        ans = []
+        length = 1
+        while(len(ans)<numRows):
+            if length==1:
+                ans.append([1])
+            elif length==2:
+                ans.append([1,1])
+            else:
+                curr = [1]
+                Sum = sum(ans[-1][:2])
+                curr.append(Sum)
+                window = 2
+                for i in range(len(ans[-1])-2):
+                    #print("Sum:",Sum)
+                    Sum += ans[-1][window]
+                    Sum -= ans[-1][i]
+                    window += 1
+                    curr.append(Sum)
+                curr.append(1)
+                ans.append(curr)
+            length += 1
+        return ans
+        
