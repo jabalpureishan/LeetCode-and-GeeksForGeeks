defb9c859b49aaf3e1b77b8515aa9d6f74030594
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-10-02 02:13:27 +0530

    Added solution - LeetHub

---
+++ Maximum no of 1's row - GFG/maximum-no-of-1s-row.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+
+class Solution:
+    def maxOnes (self, Mat, N, M):
+        return Mat.index(max(Mat,key=lambda x:x.count(1)))
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        size = input().strip().split()
+        r = int(size[0])
+		c = int(size[1])
+		line = list(map(int,input().split()))
+		matrix = [ [0 for _ in range(c)] for _ in range(r) ]
+        for i in range(r):
+			for j in range(c):
+				matrix[i][j] = line[i*c+j]
+        ob = Solution()
+        print(ob.maxOnes(matrix,r,c))
+
+# } Driver Code Ends
