07e66edc6f7a76ab56a122b4b60a054807a2d7cd
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-10-07 22:09:57 +0530

    Added solution - LeetHub

---
+++ Check whether K-th bit is set or not - GFG/check-whether-kth-bit-is-set-or-not.py
@@ -0,0 +1,42 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+# } Driver Code Ends
+#User function Template for python3
+
+
+class Solution:
+    
+    #Function to check if Kth bit is set or not.
+    def checkKthBit(self, n,k):
+        return n&(1<<k)
+
+#{ 
+ # Driver Code Starts.
+
+
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        k=int(input())
+        obj = Solution()
+        if obj.checkKthBit(n,k):
+            print("Yes")
+        else:
+            print("No")
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
