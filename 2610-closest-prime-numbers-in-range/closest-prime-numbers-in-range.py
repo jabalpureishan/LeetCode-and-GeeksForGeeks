1b3fc6481cf436913cadb8f161db7b0527ea6bc2
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2023-12-13 10:51:02 +0530

    Time: 2221 ms (47.58%) | Memory: 34.7 MB (10.57%) - LeetSync

---
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
@@ -0,0 +1,27 @@
+class Solution:
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        def sieve(N):
+                arr = [False]*2+[True]*(N-1)
+                root = sqrt(N)
+                i = 2
+                while i<=root:
+                    if arr[i]:
+                        for j in range(i*i,N+1,i):
+                            arr[j] = False
+                    i += 1
+                return arr
+            
+        # if right-left<=2:
+        #     return [-1,-1]
+        s = sieve(right)
+        ans = [-1,-1]
+        Min = float("inf")
+        prev = 0
+        for i in range(left,right+1):
+            if s[i]:
+                if prev!=0 and i-prev<Min:
+                    Min = i-prev
+                    ans = [prev,i]
+                prev = i
+        return ans
+    
