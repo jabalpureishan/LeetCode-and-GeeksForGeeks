affcb0ba7132f4ef03836f8e66120f4c52f0a782
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-01-04 11:30:10 +0530

    Time: 3280 ms (5.03%) | Memory: 163.6 MB (5.03%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -4,13 +4,15 @@ class Solution:
         to = 2**k
         fr = 0
         for i in range(fr,to):
-            set_.add(int(bin(i)[2:]))
+            now = bin(i)[2:]
+            now = "0"*(k-len(now))+now
+            set_.add(now)
         #print(set_)
         n = len(s)
         slides = n-k+1
         window = k
         for j in range(slides):
-            curr = int(s[j:window])
+            curr = s[j:window]
             set_.discard(curr)
             if len(set_)==0:
                 return True
