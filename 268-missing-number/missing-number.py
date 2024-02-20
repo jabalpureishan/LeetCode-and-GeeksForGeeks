cbbb542a88b71fae46b8b5e02350099bca3aefb8
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-02-20 12:06:38 +0530

    Time: 124 ms (29.27%) | Memory: 17.9 MB (56.92%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,11 +1,13 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        sum_,Max = 0,0
+        sum_,Max,Zero = 0,0,False
         for i in nums:
+            if i==0:
+                Zero = True
             sum_ += i
             Max = max(Max,i)
         ans = (Max*(Max+1))//2 - sum_
-        if ans==0 and 0 in set(nums):
+        if ans==0 and Zero:
             return Max + 1
         return ans
         
