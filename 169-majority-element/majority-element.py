33c4e4f6ea99d7c8e42eec5b435384f920e7f2a7
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-02-23 11:46:05 +0530

    Time: 174 ms (10.60%) | Memory: 18.1 MB (27.18%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,14 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        hmap = Counter(nums)
         n = len(nums)
-        nums.sort()
-        nums.append(nums[-1]+1)
-        count,curr = 0,nums[0]
-        for i in nums:
-            if i==curr:
-                count += 1
-            else:
-                if count>=(n/2):
-                    return curr
-                curr = i
-                count = 1
+        for i in hmap:
+            if hmap[i]>=(n/2):
+                return i
+        return None
