14800cdbecc621d12d45f8f432be9f38a627eb56
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-03-09 13:00:22 +0530

    Time: 367 ms (26.32%) | Memory: 35.4 MB (87.52%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,8 +1,13 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        nums1,nums2 = set(nums1),set(nums2)
-        nums1 = nums1.intersection(nums2)
-        if len(nums1)>0:
-            return min(nums1)
+        p1,p2 = 0,0
+        n1,n2 = len(nums1),len(nums2)
+        while p1<n1 and p2<n2:
+            if nums1[p1]<nums2[p2]:
+                p1 += 1
+            elif nums1[p1]>nums2[p2]:
+                p2 += 1
+            else:
+                return nums1[p1]
         return -1
-        
+            
