81faa735b2c3c9770db38d6dd90c2812a10aa3f2
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-03-09 12:53:33 +0530

    Time: 341 ms (84.08%) | Memory: 42.7 MB (21.26%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,11 +1,8 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        set_ = set(nums2)
-        Min = float("inf")
-        for i in nums1:
-            if i in set_:
-                Min = min(Min,i)
-        if Min==float("inf"):
-            return -1
-        return Min
+        nums1,nums2 = set(nums1),set(nums2)
+        nums1 = nums1.intersection(nums2)
+        if len(nums1)>0:
+            return min(nums1)
+        return -1
         
