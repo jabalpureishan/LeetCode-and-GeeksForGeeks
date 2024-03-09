469d25237a732327d86b296bc9fecc39af0e1fd0
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-03-09 12:15:46 +0530

    Time: 1143 ms (40.57%) | Memory: 69.9 MB (51.60%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
@@ -1,10 +1,11 @@
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        d = {}
+        heap = []
         n = len(arr)
         for i in range(n):
             for j in range(i+1,n):
-                d[(arr[i],arr[j])] = arr[i]/arr[j]
-        d = sorted(d.items(),key=lambda x:x[1])
-        return list(d[k-1][0])
+                heappush(heap,(-(arr[i] / arr[j]), arr[i], arr[j]))
+                if len(heap)>k:
+                    heappop(heap)
+        return [heap[0][1],heap[0][2]]
         
