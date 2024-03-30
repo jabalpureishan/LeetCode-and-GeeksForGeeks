0a04f342b74b89185744916f8c764409e0badf83
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-03-30 20:02:53 +0530

    Time: 878 ms (15.82%) | Memory: 32.7 MB (32.14%) - LeetSync

--- 1581-the-k-strongest-values-in-an-array/the-k-strongest-values-in-an-array.py
+++ 1581-the-k-strongest-values-in-an-array/the-k-strongest-values-in-an-array.py
@@ -1,10 +1,14 @@
 class Solution:
     def getStrongest(self, arr: List[int], k: int) -> List[int]:
+        heap = []
+        heapify(heap)
         arr.sort()
         n = len(arr)
-        
         med = arr[(n-1)//2]
-        print(med)
-        arr.sort(key=lambda x:abs(x-med))
-        return arr[-k:]
+        for i in arr:
+            heappush(heap,(abs(med-i),i))
+            if len(heap)>k:
+                heappop(heap)
+        ans = [i[1] for i in heap]
+        return ans
         
