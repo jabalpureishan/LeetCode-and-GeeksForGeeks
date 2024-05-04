02eae9ccd6395d06e7b6d4704364e764d8f3f096
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-05-04 14:43:53 +0530

    Time: 354 ms (40.82%) | Memory: 23.5 MB (25.52%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -1,17 +1,15 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        count,start,end = 0,0,len(people)-1
         people.sort()
-        #print("f",start,end)
-        while(start<=end):
-            Sum = people[start] + people[end]
-            #print(start,end)
-            if Sum<= limit:
-                count += 1
-                start += 1
-                end -= 1
-            else:
-                if people[end]<=limit:
-                    count += 1
-                end -= 1
+        p1,p2,count = 0,len(people)-1,0
+        while p1<=p2:
+            if people[p1]+people[p2]<=limit:
+                p1 += 1
+            p2 -= 1
+            count += 1
+        #print(p1,p2)
         return count
+
+
+
+        
