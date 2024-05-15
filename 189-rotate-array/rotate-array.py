def042af38ef7465b2d6a7066038899af352e636
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-05-15 22:39:55 +0530

    Time: 155 ms (73.99%) | Memory: 28.6 MB (10.15%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,10 +1,7 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        n = len(nums)
-        k = k%n
-        new = nums[-k:]
-        for i in range(n-k-1,-1,-1):
-            nums[i+k] = nums[i]
-        for i in range(k):
-            nums[i] = new[i]
+        new = deque(nums)
+        new.rotate(k)
+        nums.clear()
+        nums.extend(new)
         return nums
