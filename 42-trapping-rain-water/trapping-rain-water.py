47671425f1929865e2f7aa9fc45c27d69eace3b3
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-05-19 01:30:15 +0530

    Time: 121 ms (13.19%) | Memory: 18.8 MB (7.42%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,15 +1,15 @@
 from sortedcontainers import SortedList
 class Solution:
     def trap(self, height: List[int]) -> int:
-        prefix,suffix,p,s,n = [],deque([]),0,0,len(height)
+        prefix,suffix,p,s,n = [],[],0,0,len(height)
         for i in range(n):
             p = max(p,height[i])
             prefix.append(p)
             s = max(s,height[-(i+1)])
-            suffix.appendleft(s)
+            suffix.append(s)
         #return prefix,suffix
         total = 0
         for i in range(n):
-            total += max(0,min(prefix[i],suffix[i])-height[i])
+            total += max(0,min(prefix[i],suffix[-(i+1)])-height[i])
         return total
             
