d038a3a7eefc7b1817ebc3b93d7f242a03e9f486
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-05-24 13:37:41 +0530

    Time: 99 ms (68.99%) | Memory: 18.3 MB (98.20%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -4,12 +4,16 @@ class Solution:
         lmax,rmax,r,l,ans = 0,0,len(height)-1,0,0
         while l<r:
             if height[l]<=height[r]:
-                ans += max(0,lmax - height[l])
-                lmax = max(lmax,height[l])
+                if height[l]>=lmax:
+                    lmax = height[l]
+                else:
+                    ans += lmax - height[l]
                 l += 1
             else:
-                ans += max(0,rmax - height[r])
-                rmax = max(rmax,height[r])
+                if height[r]>=rmax:
+                    rmax = height[r]
+                else:
+                    ans += rmax - height[r]
                 r -= 1
         return ans
             
