c78e6b63628b42d6c8f4ceed3d1f4b85e0143a85
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-06-04 14:01:14 +0530

    Time: 361 ms (79.30%) | Memory: 21.7 MB (73.15%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
@@ -1,20 +1,24 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        n = len(nums)
-        TSum = sum(nums)
-        print(TSum)
-        NMax,Sum = float("-inf"),0
-        for i in nums:
-            Sum += i
-            NMax = max(NMax,Sum)
-            Sum = max(0,Sum)
-        if NMax<0:
-            return NMax
-        Sum,DMax = 0,float("inf")
-        for i in nums:
-            Sum += i
-            DMax = min(Sum,DMax)
-            Sum = min(0,Sum)
-        print(NMax,TSum,DMax)
-        return max(NMax,TSum-DMax) 
+
+        def Maximum_Sum_Subarray(nums):
+            Sum,Max = 0,float("-inf")
+            for i in nums:
+                Sum += i
+                Max = max(Max,Sum)
+                Sum = max(Sum,0)
+            return Max
+
+        def Minimum_Sum_Subarray(nums):
+            Sum,Min  = 0,float("inf")
+            for i in nums:
+                Sum += i
+                Min = min(Min,Sum)
+                Sum = min(Sum,0)
+            return Min
+
+        KDN = Maximum_Sum_Subarray(nums)
+        if KDN<0:
+            return KDN
+        return max(KDN,sum(nums) - Minimum_Sum_Subarray(nums))
         
