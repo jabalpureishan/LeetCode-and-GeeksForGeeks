82a9ab364245562350cdd276cff4a1fa9e75f8c2
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-07-26 00:01:45 +0530

    Time: 1279 ms (15.00%) | Memory: 28.6 MB (5.50%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -1,16 +1,19 @@
-from collections import OrderedDict
+from collections import defaultdict
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        main,hmap,final = {},{},[]
-        for i in nums:
-            hmap[i] =hmap.get(i,0) + 1
-        for i in nums:
-            curr,ans = str(i),""
-            for j in curr:
-                ans += str(mapping[int(j)])
-            main[i] = int(ans)
-        main = sorted(main.items(),key=lambda x:x[1])
-        for i in main:
-            final.extend([i[0]]*hmap[i[0]])
-            hmap[i[0]] -= 1
-        return final
+        d = defaultdict(list)
+        for ind,val in enumerate(nums):
+            curr = list(str(val))
+            for i,j in enumerate(curr):
+                curr[i] = str(mapping[int(j)])
+
+            curr = int("".join(curr))
+            d[curr].append(val)
+        d = sorted(d.items())
+        ans = []
+        for i in d:
+            ans.extend(i[1])
+        return ans
+                
+
+        
