934b7996826492b3d687508e9286b87a7a841ff4
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-08-06 12:27:05 +0530

    Time: 150 ms (67.54%) | Memory: 17.7 MB (8.11%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -1,11 +1,13 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        word,ans,cross,mult = Counter(word),0,7,1
+        word = Counter(word)
         word = sorted(word.items(),key=lambda x:x[1],reverse=True)
-        for i,j in enumerate(word):
-            ans += j[1]*mult
-            if i>=cross:
-                cross += 8
+        num,cost,mult = 1,0,1
+        for i in word:
+            curr = mult*i[1]
+            cost += mult*i[1]
+            if num%8==0:
                 mult += 1
-        return ans
+            num += 1
+        return cost
         
