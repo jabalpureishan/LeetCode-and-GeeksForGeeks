99be8d6c8ff2f0bcedace9ea7b8f17a4fd6a6742
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2024-08-22 01:21:38 +0530

    Time: 1351 ms (52.54%) | Memory: 41.3 MB (96.68%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
@@ -1,12 +1,18 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
-        arrays.sort(key = lambda x:x[-1])
+        Minarr,Maxarr,mn,mx = 0,0,float("inf"),float("-inf")
+        for j,i in enumerate(arrays):
+            if i[0]<mn:
+                mn = i[0]
+                Minarr = j
+            if i[-1]>mx:
+                mx = i[-1]
+                Maxarr = j
+
         Max = 0
-        for i in arrays[:-1]:
-            Max = max(Max,arrays[-1][-1]-i[0])
-        arrays.sort(key = lambda x:x[0])
-        #print(arrays)
-        for i in arrays[1:]:
-            #print(i,arrays[0][0],i[-1])
-            Max = max(Max,i[-1]-arrays[0][0])
+        for j,i in enumerate(arrays):
+            if j!=Minarr:
+                Max = max(Max,i[-1]-mn)
+            if j!=Maxarr:
+                Max = max(Max,mx-i[0])
         return Max       
