9ef642f41b84de5912309d406fbef4ecb43cd9e2
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2025-05-26 00:09:38 +0530

    Time: 478 ms (97.33%) | Memory: 29.9 MB (79.10%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -1,21 +1,19 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        hashmap = {}
+        nums.sort()
+        length = len(nums)
+        i,j = 0,length-1
         count = 0
-        for i in nums:
-            #print("i:",i)
-            #print("hashmap:",hashmap)
-            if i>=k:
-                continue
-            if i in hashmap:
-                #print("i in map")
-                count += 1
-                hashmap[i] -= 1
-                if hashmap[i]==0:
-                    hashmap.pop(i)
-                #print("after hashmap:",hashmap)
+        while i<j:
+            Sum = nums[i]+nums[j]
+            if Sum<k:
+                i += 1
+            elif Sum>k:
+                j -= 1
             else:
-                #print("i not in map")
-                hashmap[k-i] = hashmap.get(k-i,0) + 1
-                #print("after hashmap:",hashmap)
+                count += 1
+                i += 1
+                j -= 1
         return count
+
+        
