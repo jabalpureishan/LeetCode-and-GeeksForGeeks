1869ec2719eb81a9bc02e5442ad7aba59d99bbc2
Ishan Jabalpure <115293847+jabalpureishan@users.noreply.github.com>
2025-05-26 08:03:01 +0530

    Time: 147 ms (26.18%) | Memory: 19.6 MB (79.44%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -1,17 +1,19 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        def count(nums,k,n):
-            hmap,left,ans = defaultdict(int),0,0
-            for right in range(n):
+
+        def find(nums,k):
+            hmap = defaultdict(int)
+            left = count = 0
+            for right in range(len(nums)):
                 hmap[nums[right]] += 1
                 while len(hmap)>k:
                     hmap[nums[left]] -= 1
                     if hmap[nums[left]]==0:
                         hmap.pop(nums[left])
                     left += 1
-                ans += right-left+1
-            return ans
+                count += right - left + 1
+            return count
 
-        n = len(nums)
-        return count(nums,k,n)-count(nums,k-1,n)
+        return find(nums,k) - find(nums,k-1)
+
         
